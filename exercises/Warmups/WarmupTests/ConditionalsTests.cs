f551252241da033f553d1afa4471cb1e64a9c07c
Mike Drozda <mdrozda@gmail.com>
2015-09-22 12:00:51 -0400

    Conditional Unit tests 1-5

--- exercises/Warmups/WarmupTests/ConditionalsTests.cs
+++ exercises/Warmups/WarmupTests/ConditionalsTests.cs
@@ -33,5 +33,58 @@ namespace WarmupTests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestCase(false, false, true)]
+        [TestCase(true, false, false)]
+        [TestCase(false, true, true)]
+
+        public void CanSleepInTest(bool a, bool b, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.CanSleepIn(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 2, 3)]
+        [TestCase(3, 2, 5)]
+        [TestCase(2, 2, 8)]
+
+        public void SumDoubleTest(int a, int b, int expected)
+        {
+            //Act
+            int actual = _conditionals.SumDouble(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(23, 4)]
+        [TestCase(10, 11)]
+        [TestCase(21, 0)]
+
+        public void SDiff21Test(int a, int expected)
+        {
+            //Act
+            int actual = _conditionals.Diff21(a);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(true, 6, true)]
+        [TestCase(true, 7, false)]
+        [TestCase(false, 6, false)]
+
+        public void ParrotTroubleTest(bool isTalking, int hour, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.ParrotTrouble(isTalking, hour);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
--- exercises/Warmups/Warmups/Conditionals.cs
+++ exercises/Warmups/Warmups/Conditionals.cs
@@ -8,6 +8,7 @@ namespace Warmups
 {
     public class Conditionals
     {
+        // 01- MischeviousChildren
         public bool AreWeInTrouble(bool aSmile, bool bSmile)
         {
             if (aSmile == bSmile)
@@ -16,7 +17,65 @@ namespace Warmups
             }
 
             return false;
+        }
+
+        // 02 - SleepingIn
+        public bool CanSleepIn(bool isWeekday, bool isVacation)
+        {
+            if (isWeekday == false || isVacation == true)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
 
         }
+
+        // 03 - SumDouble
+        public int SumDouble(int a, int b)
+        {
+            if (a == b)
+            {
+                return (a + b)*2;
+            }
+
+            else
+            {
+                return a + b;
+            }
+        }
+
+        // 04 - Diff21
+        public int Diff21(int n)
+        {
+            if (n > 21)
+            {
+                return (n - 21)*2;
+            }
+
+            else
+            {
+                return 21 - n; 
+            }
+        }
+
+        // 05 - Parrot Trouble
+
+        public bool ParrotTrouble(bool isTalking, int hour)
+        {
+            if (isTalking == true && (hour < 7 || hour > 20))
+            {
+                return true;
+            }
+
+            else
+            {
+                return false; 
+            }
+        }
+
     }
 }
