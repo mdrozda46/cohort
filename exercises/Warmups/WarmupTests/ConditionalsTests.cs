19751cf7790ddf2e3529f41bce490446364240eb
Mike Drozda <mdrozda@gmail.com>
2015-09-24 14:35:27 -0400

    Finished conditional warmups with unit tests

--- exercises/Warmups/WarmupTests/ConditionalsTests.cs
+++ exercises/Warmups/WarmupTests/ConditionalsTests.cs
@@ -86,5 +86,264 @@ namespace WarmupTests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestCase(9, 10, true)]
+        [TestCase(9, 9, false)]
+        [TestCase(1, 9, true)]
+
+        public void Makes10Test(int a, int b, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.Makes10(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(103, true)]
+        [TestCase(90, true)]
+        [TestCase(89, false)]
+
+        public void NearHundredTest(int n, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.NearHundred(n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, -1, false, true)]
+        [TestCase(-1, 1, false, true)]
+        [TestCase(-4, -5, true, true)]
+
+        public void PosNegTest(int a, int b, bool negative, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.PosNeg(a, b, negative);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("candy", "not candy")]
+        [TestCase("x", "not x")]
+        [TestCase("not bad", "not bad")]
+
+        public void NotStringTest(string s, string expected)
+        {
+            //Act
+            string actual = _conditionals.NotString(s);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("kitten", 1, "ktten")]
+        [TestCase("kitten", 0, "itten")]
+        [TestCase("kitten", 4, "kittn")]
+
+        public void MissingCharTest(string str, int n, string expected)
+        {
+            //Act
+            string actual = _conditionals.MissingChar(str, n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("code", "eodc")]
+        [TestCase("a", "a")]
+        [TestCase("ab", "ba")]
+
+        public void FrontBackTest(string str, string expected)
+        {
+            //Act
+            string actual = _conditionals.FrontBack(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("cat", "tcatt")]
+        [TestCase("Hello", "oHelloo")]
+        [TestCase("a", "aaa")]
+
+        public void BackAroundTest(string str, string expected)
+        {
+            //Act
+            string actual = _conditionals.BackAround(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("hi there", true)]
+        [TestCase("hi", true)]
+        [TestCase(" high up", false)]
+
+        public void StartHiTest(string str, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.StartHi(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(120, -1, true)]
+        [TestCase(-1, 120, true)]
+        [TestCase(2, 120, false)]
+
+        public void IcyHotTest(int temp1, int temp2, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.IcyHot(temp1, temp2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(12, 99, true)]
+        [TestCase(21, 12, true)]
+        [TestCase(8, 99, false)]
+
+        public void Between10and20Test(int a, int b, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.Between10and20(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(13, 20, 10, true)]
+        [TestCase(20, 19, 10, true)]
+        [TestCase(20, 10, 12, false)]
+
+        public void HasTeenTest(int a, int b, int c, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.HasTeen(a, b, c);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(13, 99, true)]
+        [TestCase(21, 19, true)]
+        [TestCase(13, 13, false)]
+
+        public void SoAloneTest(int a, int b, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.SoAlone(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("adelbc", "abc")]
+        [TestCase("adelHello", "aHello")]
+        [TestCase("adedbc", "adedbc")]
+
+        public void RemoveDelTest(string str, string expected)
+        {
+            //Act
+            string actual = _conditionals.RemoveDel(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("mix snacks", true)]
+        [TestCase("pix snacks", true)]
+        [TestCase("piz snacks", false)]
+
+        public void IxStartTest(string str, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.IxStart(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("ozymandias", "oz")]
+        [TestCase("bzoo", "z")]
+        [TestCase("oxx", "o")]
+
+        public void StartOzTest(string str, string expected)
+        {
+            //Act
+            string actual = _conditionals.StartOz(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(1, 2, 3, 3)]
+        [TestCase(1, 3, 2, 3)]
+        [TestCase(3, 2, 1, 3)]
+
+        public void MaxTest(int a, int b, int c, int expected)
+        {
+            //Act
+            int actual = _conditionals.Max(a, b, c);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(8, 13, 8)]
+        [TestCase(13, 8, 8)]
+        [TestCase(13, 7, 0)]
+
+        public void CloserTest(int a, int b, int expected)
+        {
+            //Act
+            int actual = _conditionals.Closer(a, b);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Hello", true)]
+        [TestCase("Heelle", true)]
+        [TestCase("Heelele", false)]
+
+        public void GotETest(string str, bool expected)
+        {
+            //Act
+            bool actual = _conditionals.GotE(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Hello", "HeLLO")]
+        [TestCase("hi there", "hi thERE")]
+        [TestCase("hi", "HI")]
+
+        public void EndUpTest(string str, string expected)
+        {
+            //Act
+            string actual = _conditionals.EndUp(str);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("Miracle", 2, "Mrce")]
+        [TestCase("abcdefg", 2, "aceg")]
+        [TestCase("abcdefg", 3, "adg")]
+
+        public void EveryNthTest(string str, int n, string expected)
+        {
+            //Act
+            string actual = _conditionals.EveryNth(str, n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
--- exercises/Warmups/Warmups/Conditionals.cs
+++ exercises/Warmups/Warmups/Conditionals.cs
@@ -76,5 +76,292 @@ namespace Warmups
             }
         }
 
+        // 06 - Makes10
+        public bool Makes10(int a, int b)
+        {
+            if (a == 10 || b == 10 || a + b == 10)
+            {
+                return true; 
+            }
+            return false;
+        }
+
+        // 07 - NearHundred
+        public bool NearHundred(int n)
+        {
+            if (Math.Abs(n - 100) <= 10 || Math.Abs(n - 200) <= 10)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // 08 - PosNeg
+        public bool PosNeg(int a, int b, bool negative)
+        {
+            if (negative && a < 0 && b < 0)
+            {
+                return true;
+            }
+
+            else if (!negative && (a < 0 || b < 0))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        // 09 - NotString
+        public string NotString(string s)
+        {
+            string[] strArry = s.Split(' ');
+
+            if (strArry[0] == "not")
+            {
+                return s;
+            }
+
+            else
+            {
+                return "not " + s;
+            }
+        }
+
+        // 10 - MissingChar
+        public string MissingChar(string str, int n)
+        {
+            char[] charArray = str.ToCharArray();
+            string newStr = "";
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                if (i != n)
+                {
+                    newStr += charArray[i].ToString();
+                }
+            }
+
+            return newStr;
+        }
+
+        // 11 - FrontBack
+        public string FrontBack(string str)
+        {
+            string firstLetter = str.Substring(0, 1);
+            String lastLetter = str.Substring(str.Length - 1, 1);
+
+            if (str.Length == 1)
+            {
+                return str;
+            }
+            else if (str.Length == 2)
+            {
+                return lastLetter + firstLetter;
+            }
+
+            return lastLetter + str.Substring(1, str.Length - 2) + firstLetter;
+        }
+
+        // 12 - Front3
+        public string Front3(string str)
+        {
+            if (str.Length < 3)
+            {
+                return str + str + str;
+            }
+            else
+            {
+                return str.Substring(0, 3) + str.Substring(0, 3) + str.Substring(0, 3);
+            }
+        }
+
+        // 13 - BackAround
+        public string BackAround(string str)
+        {
+            return str.Substring(str.Length - 1, 1) + str + str.Substring(str.Length - 1, 1);
+        }
+
+        // 14 - Multiple3or5
+        public bool Multiple3or5(int number)
+        {
+            if (number%3 == 0 || number%5 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // 15 - StartHi
+        public bool StartHi(string str)
+        {
+            if (str.Substring(0, 2) == "hi")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // 16 - IcyHot
+        public bool IcyHot(int temp1, int temp2)
+        {
+            if (temp1 < 0 && temp2 > 100 || temp1 > 100 && temp2 < 0)
+            {
+                return true;
+            }
+            return false; 
+        }
+
+        // 17 - Between10and20
+        public bool Between10and20(int a, int b)
+        {
+            if ((a >= 10 && a <= 20) || (b >= 10 && b <= 20))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // 18 - HasTeen
+        public bool HasTeen(int a, int b, int c)
+        {
+            if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19) || (c >= 13 && c <= 19))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //19 - SoAlone
+        public bool SoAlone(int a, int b)
+        {
+            if (((a >= 13 && a <= 19) && !(b >= 13 && b <= 19)) || (!(a >= 13 && a <= 19) && (b >= 13 && b <= 19)))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //20 - RemoveDel
+        public string RemoveDel(string str)
+        {
+            if (str.Substring(1,3) == "del")
+            {
+                return str.Substring(0, 1) + str.Substring(4);
+            }
+            return str;
+        }
+
+        //21 - IxStart
+        public bool IxStart(string str)
+        {
+            if (str.Substring(1, 2) == "ix")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        //22 - StartOz
+        public string StartOz(string str)
+        {
+            string newstr = "";
+
+            if (str.Substring(0, 1) == "o")
+            {
+                newstr += "o";
+            }
+
+            if (str.Substring(1, 1) == "z")
+            {
+                newstr += "z";
+            }
+
+            return newstr;
+        }
+
+        //23 - Max
+        public int Max(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+
+            else if (b > a && b > c)
+            {
+                return b;
+            }
+
+            return c;
+
+        }
+
+        //24 - Closer
+        public int Closer(int a, int b)
+        {
+            int aDiff = Math.Abs(10 - a);
+            int bDiff = Math.Abs(10 - b);
+
+            if (aDiff == bDiff)
+            {
+                return 0;
+            }
+            
+            else if (aDiff < bDiff)
+            {
+                return a;
+            }
+
+            return b;
+
+
+        }
+
+        //25 - GotE
+        public bool GotE(string str)
+        {
+            int count = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.Substring(i, 1) == "e")
+                    count++;
+            }
+
+            if (count >= 1 && count <= 3)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        //26 - EndUp
+        public string EndUp(string str)
+        {
+            if (str.Length <= 3)
+            {
+                return str.ToUpper();
+            }
+
+            else
+            {
+                return str.Substring(0, str.Length - 3) + str.Substring(str.Length - 3, 3).ToUpper();
+            }
+        }
+
+        //27 - EveryNth
+        public string EveryNth(string str, int n)
+        {
+            string tempstr = "";
+            for (int i = 0; i < str.Length; i+=n)
+            {
+                tempstr += str[i];
+            }
+
+            return tempstr;
+        }
+
     }
 }
